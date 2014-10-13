68f528211e167e2fb5f4d8a039f24a245199de3a
LeeWayne <lee.wayne1882@googlemail.com>
2014-10-13 14:26:21 +0100

    second commit

---
+++ ninja_story/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinjaStory::Application.config.secret_token = 'c08465b2ba45e83f90824c561e250d67398d7fce569db41d8ebf78874829c2bfee93e96b175ea0a22c637106a3293a602698941e5bdc47505a1d9a0eab74c2b5'

25a1089da3ea051b557477d2d0b83b672be76e27
LeeWayne <lee.wayne1882@googlemail.com>
2014-10-17 13:55:18 +0100

    password deleted migration

--- ninja_story/db/schema.rb
+++ ninja_story/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141017115641) do
+ActiveRecord::Schema.define(:version => 20141017125434) do
 
   create_table "choices", :force => true do |t|
     t.datetime "created_at",  :null => false
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(:version => 20141017115641) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.string   "password"
     t.string   "ninja_name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false

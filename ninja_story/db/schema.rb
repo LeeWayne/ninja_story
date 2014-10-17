26e57ed903f9584bc50fea12d724baec8081dff0
LeeWayne <lee.wayne1882@googlemail.com>
2014-10-17 14:59:35 +0100

    removed image

--- ninja_story/db/schema.rb
+++ ninja_story/db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(:version => 20141017125704) do
   end
 
   create_table "slides", :force => true do |t|
-    t.string   "image"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "story_id"

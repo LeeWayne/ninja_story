7253d80d63154271b1930437223e364572681548
LeeWayne <lee.wayne1882@googlemail.com>
2014-10-20 09:12:57 +0100

    models and seeds changed

--- ninja_story/app/models/choice.rb
+++ ninja_story/app/models/choice.rb
@@ -1,6 +1,6 @@
 class Choice < ActiveRecord::Base
-  attr_accessible :start_slide, :end_slide, :description
+  attr_accessible :start_slide_id, :end_slide_id, :description
 
-  belongs_to :start_slide, class_name: "Slide"
-  belongs_to :end_slide, class_name: "Slide"
+  belongs_to :start_slide_id, class_name: "Slide"
+  belongs_to :end_slide_id, class_name: "Slide"
 end

29e473f43817556bb1b8c6613496206271794997
LeeWayne <lee.wayne1882@googlemail.com>
2014-10-20 15:59:13 +0100

    added images to seed

--- ninja_story/db/seeds.rb
+++ ninja_story/db/seeds.rb
@@ -15,53 +15,53 @@ Story.create(sypnosis:"The sacred Goblet of your village has been stolen!! It’
 
 Slide.create(id: 1, image: "backdrops/Slide_1_hi_res.png", story_id: 1, description: "You leave the village and you come across a rickety bridge do you..")
 
-Slide.create(id: 2, image: "backdrops/Slide_1_hi_res.png", story_id: 1, description: "The bridge falls behind you but you made it! You stop for a second to collect some water from the well! Whoa, there’s a goblin, do you..")
+Slide.create(id: 2, image: "backdrops/Slide_2_hi_res.png", story_id: 1, description: "The bridge falls behind you but you made it! You stop for a second to collect some water from the well! Whoa, there’s a goblin, do you..")
 
-Slide.create(id: 3, image: "backdrops/Slide_1_hi_res.png", story_id: 1, description: "Great jump! Whoa, there’s a rival ninja, do you..")
+Slide.create(id: 3, image: "backdrops/Slide_3_hi_res.png", story_id: 1, description: "Great jump! Whoa, there’s a rival ninja, do you..")
 
-Slide.create(id: 4, image: "backdrops/Slide_1_hi_res.png", story_id: 1, description: "You bored him unconscious! You see a cloak on a tree and pick it up. Look, spikes are blocking your way, what are you going to do…")
+Slide.create(id: 4, image: "backdrops/Slide_4_hi_res.png", story_id: 1, description: "You bored him unconscious! You see a cloak on a tree and pick it up. Look, spikes are blocking your way, what are you going to do…")
 
-Slide.create(id: 5, image: "", story_id: 1, description: "Great ninja skills! You pick up a freezing potion dropped by the ninja! You see a dark, creepy looking cave with two openings, what now ninja??")
+Slide.create(id: 5, image: "backdrops/Slide_5_hi_res.png", story_id: 1, description: "Great ninja skills! You pick up a freezing potion dropped by the ninja! You see a dark, creepy looking cave with two openings, what now ninja??")
 
-Slide.create(id: 6, image: "", story_id: 1, description: "A great ninja always fights with words first! You see a rusty old flute and pick it up. Uh oh, a giant, evil snake snake blocks your path, what now ninja?")
+Slide.create(id: 6, image: "backdrops/Slide_6_hi_res.png", story_id: 1, description: "A great ninja always fights with words first! You see a rusty old flute and pick it up. Uh oh, a giant, evil snake snake blocks your path, what now ninja?")
 
-Slide.create(id: 7, image: "", story_id: 1, description: "OH NO!!!!! THE BRIDGE COLLAPSES!!! Your Ninja skills don’t include building bridges…...THE END Please try again to save the sacred Goblet!")
+Slide.create(id: 7, image: "backdrops/Slide_7,9,11,13,15,17,19,22_hi_res.png", story_id: 1, description: "OH NO!!!!! THE BRIDGE COLLAPSES!!! Your Ninja skills don’t include building bridges…...THE END Please try again to save the sacred Goblet!")
 
-Slide.create(id: 8,image: "", story_id: 1, description: "Wow, what a fantastic leap, we’re sure to save the village...Frank, a one eyed, grumpy troll, stops you in your path, what to do….")
+Slide.create(id: 8,image: "backdrops/Slide_8_hi_res.png", story_id: 1, description: "Wow, what a fantastic leap, we’re sure to save the village...Frank, a one eyed, grumpy troll, stops you in your path, what to do….")
 
-Slide.create(id: 9, image: "", story_id: 1, description: "AAAHHHHHHHH!!!! The spiders cover you from head to toe and you can’t get away!! THE END Please try again to save the sacred Goblet!")
+Slide.create(id: 9, image: "backdrops/Slide_7,9,11,13,15,17,19,22_hi_res.png", story_id: 1, description: "AAAHHHHHHHH!!!! The spiders cover you from head to toe and you can’t get away!! THE END Please try again to save the sacred Goblet!")
 
-Slide.create(id: 10, image: "", story_id: 1, description: "You ducked and weaved past the bats like the Ninja you are and made it through the other side! An old, wise mage is floating in your way, the mage seem’s nice, doesn’t it??? Your choice…")
+Slide.create(id: 10, image: "backdrops/Slide_10_hi_res.png", story_id: 1, description: "You ducked and weaved past the bats like the Ninja you are and made it through the other side! An old, wise mage is floating in your way, the mage seem’s nice, doesn’t it??? Your choice…")
 
-Slide.create(id: 11, image: "", story_id: 1, description: "CHOMP!!! The snake is quicker than you ninja, and gobbles you up! THE END
+Slide.create(id: 11, image: "backdrops/Slide_7,9,11,13,15,17,19,22_hi_res.png", story_id: 1, description: "CHOMP!!! The snake is quicker than you ninja, and gobbles you up! THE END
 Please try again to save the sacred Goblet!")
 
-Slide.create(id: 12, image: "", story_id: 1, description: "WOW! Your hypnotising eye’s sent the snake into a deep sleep! This doesn't look good, you see a dark, terrifying spirit. What now ninja..")
+Slide.create(id: 12, image: "backdrops/Slide_12_hi_res.png", story_id: 1, description: "WOW! Your hypnotising eye’s sent the snake into a deep sleep! This doesn't look good, you see a dark, terrifying spirit. What now ninja..")
 
-Slide.create(id: 13, image: "", story_id: 1, description: "STOMP!! Frank hopped from his boulders and squished you…..THE END Please try again to save the sacred Goblet!")
+Slide.create(id: 13, image: "backdrops/Slide_7,9,11,13,15,17,19,22_hi_res.png", story_id: 1, description: "STOMP!! Frank hopped from his boulders and squished you…..THE END Please try again to save the sacred Goblet!")
 
-Slide.create(id: 14, image: "", story_id: 1, description: "Turns out he was only grumpy because he was cold, great thinking!! AHHHHH!! 
+Slide.create(id: 14, image: "backdrops/Slide_14,16,18_hi_res.png", story_id: 1, description: "Turns out he was only grumpy because he was cold, great thinking!! AHHHHH!! 
 The legends are true, the mythical dragon, IFRIT, is alive and he has the Goblet!!! What are YOU going to do Ninja??")
 
-Slide.create(id: 15, image: "", story_id: 1, description: "FREEZE!! The mage petrifies you in your path, you can no longer move ninja…
+Slide.create(id: 15, image: "backdrops/Slide_7,9,11,13,15,17,19,22_hi_res.png", story_id: 1, description: "FREEZE!! The mage petrifies you in your path, you can no longer move ninja…
 THE END Please try again to save the sacred Goblet!
 ")
 
-Slide.create(id: 16, image: "", story_id: 1, description: "Well done. The mage was once a member of your village but was cursed by 
+Slide.create(id: 16, image: "backdrops/Slide_14,16,18_hi_res.png", story_id: 1, description: "Well done. The mage was once a member of your village but was cursed by 
 a mythical creature and fled. She understands you and lets you past. The legends are true, the mythical dragon, IFRIT, is alive and he has the Goblet!!! What are YOU going to do Ninja??")
 
-Slide.create(id: 17, image: "", story_id: 1, description: "WHOOSH! The evil spirit sucks you up and throws you back to the beginning… THE END Please try again to save the sacred Goblet!")
+Slide.create(id: 17, image: "backdrops/Slide_7,9,11,13,15,17,19,22_hi_res.png", story_id: 1, description: "WHOOSH! The evil spirit sucks you up and throws you back to the beginning… THE END Please try again to save the sacred Goblet!")
 
-Slide.create(id: 18, image: "", story_id: 1, description: "The magical song you play turns the evil spirit back into the white, good spirit she once was! The legends are true, the mythical dragon, IFRIT, is alive and he has the Goblet!!! What are YOU going to do Ninja??")
+Slide.create(id: 18, image: "backdrops/Slide_14,16,18_hi_res.png", story_id: 1, description: "The magical song you play turns the evil spirit back into the white, good spirit she once was! The legends are true, the mythical dragon, IFRIT, is alive and he has the Goblet!!! What are YOU going to do Ninja??")
 
-Slide.create(id: 19, image: "", story_id: 1, description: "WHAT WERE YOU THINKING NINJA?? A small amount of water cannot defeat a beast as large as Ifrit! THE END Please try again to save the sacred Goblet!")
+Slide.create(id: 19, image: "backdrops/Slide_7,9,11,13,15,17,19,22_hi_res.png", story_id: 1, description: "WHAT WERE YOU THINKING NINJA?? A small amount of water cannot defeat a beast as large as Ifrit! THE END Please try again to save the sacred Goblet!")
 
 Slide.create(id: 20, image: "", story_id: 1, description: "CONGRATULATIONS!!!!!!! You really are a true ninja, you super kicked IFRIT out into the night sky!! You have the Goblet and have brought peace and harmony back to your village!! WELL DONE!!")
 
 Slide.create(id: 21, image: "", story_id: 1, description: "SMASH!! You throw the potion at IFRIT and it smashes against his scaly body and freezes him into eternity!!! CONGRATULATIONS!!!!!!! You have the Goblet and have brought peace and harmony back to your village!!
 ")
 
-Slide.create(id: 22, image: "", story_id: 1, description: "AHHHH, OUCH!! Your puney, weak, ninja skills are no match for the magnificent IFRIT YOU LOSE THE END Please try again to save the sacred Goblet!")
+Slide.create(id: 22, image: "backdrops/Slide_7,9,11,13,15,17,19,22_hi_res.png", story_id: 1, description: "AHHHH, OUCH!! Your puney, weak, ninja skills are no match for the magnificent IFRIT YOU LOSE THE END Please try again to save the sacred Goblet!")
 
 Slide.create(id: 23, image: "", story_id: 1, description: "We hear you NINJA!!
 The spirits of ninjas past, bestow you with their powers and you blast the feeble IFRIT out of Earths orbit!! Ninja’s always look after one another…..CONGRATULATIONS You have the Goblet and have brought peace and harmony back to your village!! THE END")

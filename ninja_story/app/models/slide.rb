a02a8b24ff216c95176eaec04b4683e41ee0792b
LeeWayne <lee.wayne1882@googlemail.com>
2014-10-20 10:34:53 +0100

    fixing choices

--- ninja_story/app/models/slide.rb
+++ ninja_story/app/models/slide.rb
@@ -2,7 +2,7 @@ class Slide < ActiveRecord::Base
   attr_accessible :image, :story_id, :description
 
   belongs_to :story
-  has_many :choices, as: :start_slide_id
-  has_many :choices, as: :end_slide_id
+  has_many :choices, foreign_key: "start_slide_id"
+
 
 end
